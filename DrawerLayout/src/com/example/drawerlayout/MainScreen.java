b67e8161f2d2731d0c21f4d34d743c41b16354c7
karandhamija <karandhamija@yahoo.co.in>
2015-05-25 22:05:18 +0530

    Removal of unnecessary code

--- DrawerLayout/src/com/example/drawerlayout/MainScreen.java
+++ DrawerLayout/src/com/example/drawerlayout/MainScreen.java
@@ -18,41 +18,6 @@ public class MainScreen extends Activity implements FragmentCallback {
 		FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction mTransaction = fragmentManager.beginTransaction();
         mTransaction.add(R.id.textView1, LayoutFragment.newInstance()).commit();
-
-		/*View.OnClickListener mNewListener = new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				String mTab = "";
-				switch(v.getId()){
-				case R.id.textView1 :
-					mTab = "1";
-					break;
-				case R.id.textView2 :
-					mTab = "2";
-					break;
-				case R.id.textView3 :
-					mTab = "3";
-					break;
-				case R.id.textView4 :
-					mTab = "4";
-					break;
-				}
-				Intent mIntent = new Intent();
-				mIntent.setClass(getApplicationContext(), MainActivity.class);
-				mIntent.setData(Uri.parse(mTab));
-				startActivity(mIntent);
-			}
-		};
-		TextView mView1 = (TextView) findViewById(R.id.textView1);
-		mView1.setOnClickListener(mNewListener);
-		TextView mView2 = (TextView) findViewById(R.id.textView2);
-		mView2.setOnClickListener(mNewListener);
-		TextView mView3 = (TextView) findViewById(R.id.textView3);
-		mView3.setOnClickListener(mNewListener);
-		TextView mView4 = (TextView) findViewById(R.id.textView4);
-		mView4.setOnClickListener(mNewListener);*/
 	}
 
 	@Override
